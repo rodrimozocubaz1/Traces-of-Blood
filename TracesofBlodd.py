5010001e768ff1ae59900b951f228fc633458b16
Ricardo Marquez <ricardux.13.2001@gmail.com>
2018-10-24 08:45:03 -0500

    iniciacion de codigos

--- TracesofBlodd.py
+++ TracesofBlodd.py
@@ -1,7 +1,39 @@
-b=0
-c=6
+# Coloca el código de tu juego en este archivo.
+
+# Declara los personajes usados en el juego como en el ejemplo:
+
+define e = Character("detective")
 
 
-a= b+c
+# El juego comienza aquí.
 
-print (a)
+label start:
+     # Tiempo Límite
+    $ tiempo = 20
+    
+    # Medidor de vida
+    $ Vida = 3
+
+    #$ renpy.music.play ("sounds/suspenso.mp3")
+    
+    # Muestra una imagen de fondo: Aquí se usa un marcador de posición por
+    # defecto. Es posible añadir un archivo en el directorio 'images' con el
+    # nombre "bg room.png" or "bg room.jpg" para que se muestre aquí.
+
+    scene bg room
+
+    # Muestra un personaje: Se usa un marcador de posición. Es posible
+    # reemplazarlo añadiendo un archivo llamado "eileen happy.png" al directorio
+    # 'images'.
+
+    show eileen happy
+
+    # Presenta las líneas del diálogo.
+
+    e "Bienvenido a tu primera misión después de graduarte de la academia de detectives "
+
+    e "Añade una historia, imágenes y música, ¡y puedes presentarlo al mundo!"
+
+    # Finaliza el juego:
+
+    return
