d3628687f6d1e79edd1f0c819f7a5ba5cd546b21
alumno informatica <alumno@ciis.usil.edu.pe>
2018-10-30 08:35:32 -0500

    "AVANZANDO LABEL HOMICIDIO"

--- TracesofBlodd.py
+++ TracesofBlodd.py
@@ -64,8 +64,12 @@ label start:
         return
         
     label homicidio:
-        e"dhdgh"
-
+        e"Suerte en tu misión, porque la necestirás"
+        #Se muestran las imagenes de la historia
+        scene golpe
+        with fade
+        
+        d"Largate de mi vida"
         # Finaliza el juego:
 
         return
