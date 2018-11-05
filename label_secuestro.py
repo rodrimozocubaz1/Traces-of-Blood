ce88c60f6fd26a2d74d5c6e58e110546aa177f17
Ricardo Marquez <lasuerteloestodo@gmail.com>
2018-11-04 19:18:25 -0500

    continuando la historia(incompleto)

--- label_secuestro.py
+++ label_secuestro.py
@@ -48,7 +48,7 @@ label secuestro:
     $ povname = renpy.input("¿Cuál es tu nombre?")
     dn "Mi nombre es [dn]"
     
-    cn"Bueno %(povname)s ahora que hemos terminado de presentarnos daremos inicio a la investigación"
+    cn"Bueno [dn] ahora que hemos terminado de presentarnos daremos inicio a la investigación"
     cn"¿Con qué deberíamos empezar?"
     
     dn"Creo que deberiamos investigar el lugar donde el presidente estuvo alojado el día anterior al secuestro
@@ -59,7 +59,7 @@ label secuestro:
     
     "Después de eso nos dirigimos hacia el hotel en el coche de Teresa, cuando llegamos inmediatamente nos pusimos a investigar las instalaciones
      y el cuarto de la víctima pero no hubo resultados positivos"
-    "Horas más tarde pedimos rentamos la misma habitación en donde se hospedó el Presidente"
+    "Horas más tarde rentamos la misma habitación en donde se hospedó el Presidente"
     
     # habitacion hotel 
     
@@ -99,7 +99,136 @@ label secuestro:
     
     dn"Sólo es una corazonada, pero si se da el caso, debemos estar preparados para lo peor"
     
+    cn"Entonces, ¿Qué hacemos?"
     
+    dn"Esto es lo que haremos"
+    
+    "Me acerco a ella y le digo en el oído todo el plan"
+    
+    cn"Ya veo, pero será peligroso"
+    
+    dn"Descuida, aunque no lo creas soy fuerte"
+    
+    cn"Está bien, confiaré en ti"
+    
+    "Media hora después Teresa dejó la habitación en la que nos encontrábamos"
+    
+    dn"Bueno, ¿Y ahora que hago?"
+    
+    menu:
+        
+        "Dormir para recargar energía":
+            $flag="dormir"
+            jump muerte1
+            
+        "Quedarme haciendo guardia toda la noche":
+            $flag="plan"
+            jump continuar1
+            
+#-------------------------------------------------------------------------------------------------------------------------------------------------------            
+    if flag=="dormir":
+        label muerte1:
+            dn"Creo que dormiré por el momento, despuésde todo no hay nada mejor que hacer"
+            
+            "Me tiré a la cama e inmediatamente me quedé dormido,sin preocuparme por nada, fui un estúpido..."
+            
+            "Al día siguiente Teresa me encontró muerto, con un cuchillo enterrado en mi corazón"
+            
+            # pantalla oscura
+            
+            e"Moriste, ¿Quieres volver a intentarlo?"
+            
+            menu:
+                "Sí":
+                    $flag="sí"
+                    
+                "No":
+                    return
+                    
+            if flag=="sí":
+                
+                dn"Bueno, ¿Y ahora que hago?"
+                
+                menu:
+        
+                    "Dormir para recargar energía":
+                        $flag="dormir"
+                        jump muerte1
+                        
+                    "Quedarme haciendo guardia toda la noche":
+                        $flag="plan"
+                        jump continuar1
+                        
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------    
+    if flag=="plan":
+        label continuar1:
+            dn"Creo que me quedaré despierto, no quiero que alguien me agarre con la guardia baja"
+            
+            "Pasaron la horas pero nada ocurría, todo estaba muy silencioso y oscuro, tan oscuro que me hacía pensar que estaba ciego"
+            "Ya no podía más, mis párpados ya no aguantaban las ganas de caer rendidos"
+            "Cada vez que pareciese que cayecen, yo forzosamente trataba de levantarlos, era una batalla contra el sueño"
+            "Y así pasaron las horas..."
+            "Hasta que de pronto escuché un ruido por la cocina, ese ruido me puso inmediatamente en guardia, todo mi sueño se había esfumado"
+            "Se escuchaban los pasos de una persona, recorriendo por toda la sala"
+            
+            dn"¿Qué es lo que hago ahora?"
+            
+            menu:
+                
+                "Buscar algún objeto con el cual poder defenderme":
+                    $flag="defensa"
+                    jump continuar2
+                    
+                "Caminar sigilosamente hacia el sonido de los pasos en la sala":
+                    $flag="chisme"
+                    jump muerte2
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------                    
+            if flag=="chisme":
+                label muerte2:
+                    "Caminé hacia el sonido sin hacer algún tipo de ruido, caminé lo más ligero posible para no alertar al usurpador de mi llegada"
+                    "Al llegar a la entrada a la sala sentí que la presión que sentía comenzaba a aumentar pero ya era muy tarde para retroceder"
+                    "Gire la manija y abrí la puerta muy lentamente, solo moví la puerta a un espacio para que puediera mirar con un ojo"
+                    "Procedí a acercar mi cara al espacio entre mi cuarto y la sala, cuando finalmente podía ver hacia el otro lado toda mi vista de la parte
+                     derecha se volvió oscura"
+                    
+                    dn"Eh?...{w=2}{size=+9}¡¡¡¡¡¡AAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHH!!!!!!{/size}"
+                    
+                    "Cuando me di cuenta de lo que había pasado pegué un grito de desesperación y caí al suelo"
+                    "Me habían clavado un cuchillo en el ojo derecho, mis prendas estaban manchadas por mi sangre, no podía creerlo, toda esa mancha roja 
+                     era mi sangre"
+                    "Se escuchaban las pisadas en la sala mucho... mucho más cerca a mi ubicación, el sujeto abrió la puerta rápidamente con un arma en mano
+                     y sin siquiera dudarlo apretó el gatillo una y otra y otra y otra y otra y otra y otra y otra vez hasta quedarse sin balas"
+                    "Sin siquiera ver el rostro del culpable morí"
+                    
+                    e"¿Quieres volver a intentarlo?"
+                    
+                    menu:
+                        
+                        "Sí":
+                            $flag="sí1"
+                            
+                        "No":
+                            return
+                            
+                    if flag=="sí1":
+                        dn"¿Qué es lo que hago ahora?"
+            
+                        menu:
+                
+                            "Buscar algún objeto con el cual poder defenderme":
+                                $flag="defensa"
+                                jump continuar2
+                                
+                            "Caminar sigilosamente hacia el sonido de los pasos en la sala":
+                                $flag="chisme"
+                                jump muerte2
+                        
+            if flag=="defensa":
+                label continuar2:
+                    "Caminé sigilosamente hacia mis cajones porque ahí se encontraba el arma que me había entregado Teresa antes de irse"
+                    #mostrar flashback
+                            
+                
     
     
     # Finaliza el juego:
